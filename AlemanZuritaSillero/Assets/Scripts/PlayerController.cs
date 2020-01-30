0413f91ea234f331383ccf2e8d4406be52632c47
SonicFoxPart2 <46996654+SonicFoxPart2@users.noreply.github.com>
2020-01-30 13:03:53 +0100

    animations

---
+++ AlemanZuritaSillero/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterStates currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeState(new GroundedState(this));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState.Execute();
+    }
+    public void ChangeState(CharacterStates newState)
+    {
+        if (newState != null)
+        {
+            if (currentState != null)
+                currentState.OnFinish();
+            currentState = newState;
+            currentState.OnFinish();
+        }
+    }
+}
